f189e76f0b229f62d70943314056e0eabbaafab9
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-09-03 14:52:32 +0200

    feat(projects.js): storing the image url in a variable

--- src/assets/content/projects.js
+++ src/assets/content/projects.js
@@ -1,9 +1,11 @@
+import samsungFoodImage from "../image/samsung-food.jpg";
+
 export const projects = [
   {
     id: 1,
     title: "Samsung Food Replica",
-    image: "../src/assets/image/samsung-food.jpg",
-    liveDemo: "https:www.linkedin.com/in/hamada-abdelaal-67aa9b223", // My LinkedIn Profile for now
-    details: "Detailed information about Mu final project",
+    image: samsungFoodImage, // Use the imported image
+    liveDemo: "https://www.linkedin.com/in/hamada-abdelaal-67aa9b223", // Corrected link with https://
+    details: "Detailed information about My final project",
   },
 ];
