3957b0272c93431ce7505a151199c8cd2b2d7f0c
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-09-09 11:08:04 +0200

    feat(Contact): update, complete contact's details and adding translation

--- src/components/Cnotact.jsx
+++ src/components/Cnotact.jsx
@@ -1,4 +1,8 @@
 import { Element } from "react-scroll";
+import { formIcon } from "../assets/icon/contact";
+import parse from "html-react-parser";
+
+// Components
 import ContactForm from "./ContactForm";
 
 export default function Contact({ language }) {
@@ -15,18 +19,62 @@ export default function Contact({ language }) {
         </div>
         <div className="contact-body">
           <div className="contact-intro">
-            <p>
-              {language === "EN"
-                ? "I'd love to hear from you! Whether you have questions or feedback, feel free to reach out. Use the contact form to send a message directly. Fill in your details - email and message are required. Your message will be sent securely. Alternatively, connect with me on LinkedIn or other social platforms linked below. Looking forward to hearing from you!"
-                : "Mi piacerebbe sentire da te! Che tu abbia domande o feedback, non esitare a contattarmi. Usa il modulo di contatto per inviare un messaggio direttamente. Inserisci i tuoi dati - email e messaggio sono obbligatori. Il tuo messaggio sarà inviato in modo sicuro. In alternativa, puoi connetterti con me su LinkedIn o su altre piattaforme social linkate qui sotto. Non vedo l'ora di sentirti!"}
-            </p>
-
-            <div className="platforms">
-              <div>1</div>
-              <div>2</div>
-              <div>3</div>
-              <div>3</div>
+            <div>
+              <h4>{language === "EN" ? "Get in Touch" : "Contattami"} </h4>
+              <p>
+                {language === "EN"
+                  ? "I'd love to hear from you! Whether you have questions, or feedbackor just want to say hello, feel free to reach out. Looking forward to hearing from you!"
+                  : "Sarei felice di sentirti! Che tu abbia domande, feedback o semplicemente voglia dire ciao, non esitare a contattarmi. Non vedo l'ora di sentirti!"}
+              </p>
             </div>
+
+            <div>
+              <h4>Email</h4>
+              {language === "EN" ? (
+                <p>
+                  Prefer to send an email? Drop me a message directly at{" "}
+                  <a href="mailto:hamada_abdelaal@hotmail.com">
+                    hamada_abdelaal@hotmail.com
+                  </a>
+                  , and I’ll get back to you as soon as possible.
+                </p>
+              ) : (
+                <p>
+                  Preferisci inviare un'email? Mandami un messaggio direttamente
+                  a{" "}
+                  <a href="mailto:hamada_abdelaal@hotmail.com">
+                    hamada_abdelaal@hotmail.com
+                  </a>{" "}
+                  e ti risponderò il prima possibile.
+                </p>
+              )}
+            </div>
+
+            <div>
+              <h4>{language === "EN" ? "Location" : "Posizione"}</h4>
+              <p>
+                {language === "EN"
+                  ? "Terno d'Isola, Italy"
+                  : "Terno d'Isola, Italia"}
+              </p>
+            </div>
+
+            <ul className="platforms">
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/hamada-abdelaal"
+                  target="_blank"
+                >
+                  {parse(formIcon.linkedIn)}
+                </a>
+              </li>
+
+              <li>
+                <a href="https://github.com/Hamada-AB" target="_blank">
+                  {parse(formIcon.gitHub)}
+                </a>
+              </li>
+            </ul>
           </div>
 
           <ContactForm />
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -58,6 +58,7 @@ export default function ContactForm() {
           });
 
           setShow(true);
+          setError("");
         } else {
           setError("Failed to send message.");
         }
