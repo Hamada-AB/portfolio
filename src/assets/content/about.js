5cfa4d52e6018b2909c58f6c7dad428f457d372b
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-09-15 08:47:09 +0200

    feat(About): store about's content in bio object

---
+++ src/assets/content/about.js
@@ -0,0 +1,27 @@
+export const bio = {
+  p1: {
+    en: `Hello there! My name is Hamada Abdelaal. I&#39;m a junior
+         full-stack developer based in Italy. I recently completed a
+         six-month coding bootcamp at <a href="https://boolean.co.uk/" target="_blank">Boolean&nbsp;UK</a>, where I honed my skills in a variety of technologies. My tech stack includes HTML, CSS, JavaScript, React, Node.js,Express.js, Prisma, and PostgreSQL. I&#39;m passionate about creating efficient and user-friendly web applications and eager to apply my skills in real-world projects.`,
+    it: `Ciao! Mi chiamo Hamada Abdelaal. Sono uno sviluppatore full-stack junior con sede in Italia. Ho recentemente completato un bootcamp di programmazione di sei mesi presso <a href="https://boolean.co.uk/" target="_blank">Boolean&nbsp;UK</a>, dove ho affinato le mie competenze in una varietà di tecnologie. Il mio stack tecnologico include HTML, CSS, JavaScript, React, Node.js, Express.js, Prisma e PostgreSQL. Sono appassionato di creare applicazioni web efficienti e user-friendly e sono desideroso di applicare le mie competenze in progetti del mondo reale.`,
+  },
+  p2: {
+    en: `My professional journey has been diverse. I spent a decade
+        in accounting, which refined my patience and
+        precision-skills essential for identifying calculation
+        errors. After that, I explored various other fields before
+        transitioning into technology. This varied experience has
+        broadened my perspective and honed my problem-solving
+        abilities. These diverse competencies have proven to be
+        invaluable assets in my new career as a developer.`,
+    it: `Il mio percorso professionale è stato variegato. Ho
+          trascorso un decennio nel settore contabile, che ha affinato
+          la mia pazienza e precisione-competenze essenziali per
+          identificare errori di calcolo. Successivamente, ho
+          esplorato vari altri campi prima di passare alla tecnologia.
+          Questa esperienza diversificata ha ampliato la mia
+          prospettiva e affinato le mie capacità di problem-solving.
+          Queste competenze diverse si sono rivelate risorse
+          inestimabili nella mia nuova carriera come sviluppatore.`,
+  },
+};
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { Element } from "react-scroll";
 import portfolioImg from "../assets/image/my-photo.jpg";
-
+import { bio } from "../assets/content/about";
 // Components
 import TechCarousel from "./TechCarousel";
 
@@ -66,63 +66,13 @@ export default function About({ language, winWidth }) {
 
           <div className="bio">
             {language === "EN" ? (
-              <p>
-                Hello there! My name is Hamada Abdelaal. I&#39;m a junior
-                full-stack developer based in Italy. I recently completed a
-                six-month coding bootcamp at{" "}
-                <a href="https://boolean.co.uk/" target="_blank">
-                  {" "}
-                  Boolean&nbsp;UK
-                </a>
-                , where I honed my skills in a variety of technologies. My tech
-                stack includes HTML, CSS, JavaScript, React, Node.js,
-                Express.js, Prisma, and PostgreSQL. I&#39;m passionate about
-                creating efficient and user-friendly web applications and eager
-                to apply my skills in real-world projects.
-              </p>
+              <p dangerouslySetInnerHTML={{ __html: bio.p1.en }} />
             ) : (
-              <p>
-                Ciao! Mi chiamo Hamada Abdelaal. Sono uno sviluppatore
-                full-stack junior con sede in Italia. Ho recentemente completato
-                un bootcamp di programmazione di sei mesi presso{" "}
-                <a href="https://boolean.co.uk/" target="_blank">
-                  {" "}
-                  Boolean&nbsp;UK
-                </a>
-                , dove ho affinato le mie competenze in una varietà di
-                tecnologie. Il mio stack tecnologico include HTML, CSS,
-                JavaScript, React, Node.js, Express.js, Prisma e PostgreSQL.
-                Sono appassionato di creare applicazioni web efficienti e
-                user-friendly e sono desideroso di applicare le mie competenze
-                in progetti del mondo reale.
-              </p>
+              <p dangerouslySetInnerHTML={{ __html: bio.p1.it }} />
             )}
             {language === "EN"
-              ? showFullText && (
-                  <p>
-                    My professional journey has been diverse. I spent a decade
-                    in accounting, which refined my patience and
-                    precision-skills essential for identifying calculation
-                    errors. After that, I explored various other fields before
-                    transitioning into technology. This varied experience has
-                    broadened my perspective and honed my problem-solving
-                    abilities. These diverse competencies have proven to be
-                    invaluable assets in my new career as a developer.
-                  </p>
-                )
-              : showFullText && (
-                  <p>
-                    Il mio percorso professionale è stato variegato. Ho
-                    trascorso un decennio nel settore contabile, che ha affinato
-                    la mia pazienza e precisione-competenze essenziali per
-                    identificare errori di calcolo. Successivamente, ho
-                    esplorato vari altri campi prima di passare alla tecnologia.
-                    Questa esperienza diversificata ha ampliato la mia
-                    prospettiva e affinato le mie capacità di problem-solving.
-                    Queste competenze diverse si sono rivelate risorse
-                    inestimabili nella mia nuova carriera come sviluppatore.
-                  </p>
-                )}
+              ? showFullText && <p>{bio.p2.en}</p>
+              : showFullText && <p>{bio.p2.it}</p>}
             <button onClick={toggleText}>
               {showFullText
                 ? language === "EN"
